23cdb2d32597115ca7a013a27e9faa240f7acb03
Kalinsky <reckkbg@gmail.com>
2018-04-26 13:55:16 +0300

    ADDED NEW THINGs , BRUH

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,58 @@
-var meStuff = ["Gamer", "Full Stack Web Developer", "Bearded guy", "Coca-Cola lover", "World of Warcraft fan", "MCU Fan"];
+var meStuff = ["Test6", "Test1", "Test2", "Test3", "Test4", "Test5"];
+
 var count = 0;
 $(document).ready(function() {
   animateH3();
   slidePage();
+
+  meStuff.forEach(item => {
+    console.log(item);
+  });
+
+  var cats = {
+    cat1: 'Tomi',
+    cat2: "Tomas",
+    cat3: "Minka",
+    cat4: "Vushka",
+    "Patka": 55 
+  };
+
+
+
+  for (var item in cats) {
+    console.log(cats[item]);
+  }
+
+  console.log(cats["Patka"]);
+
+  
+
 });
 
 
 //Change h3 over time
 function animateH3() {
   var changeH3 = setInterval(function(){
-    if(count === 6) {
+    if (count === 6) {
       clearInterval(changeH3);
     }
+
     count++;
     $("#changer").text(meStuff[count % meStuff.length]).fadeIn(500).delay(500);
   }, 2000);  
 }
 
 function slidePage() {
-  $('span').on('click', function(event){
-    event.preventDefault();
+  $('.slideAbout').on('click', function(event){
+    console.log("ASDASDASD");
+    var scrolll = $(window).scrollTop();
+    var divHeight = $(".section").height();
+    var finalka = Math.floor(scrolll / divHeight);
     $('html, body').animate({
-      scrollTop: $("#hereme").offset().top
+      scrollTop: $($('.section')[finalka + 1]).offset().top
     }, 1500);
-
-  })
+  });
+  console.log($(this));
   
 }
+
